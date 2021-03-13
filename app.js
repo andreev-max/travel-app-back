40e3a379d127901eec1dd2215335c3ffeb1d0b5b
nAzdAc <nAzdAc15071996@mail.ru>
2021-03-13 20:53:27 +0300

    feat: add routes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json({ extended: true }))
-
+app.use(cors());
 app.use('/api/auth', require('./routes/auth.routes'))
 
 const PORT = config.get('PORT') || 8080;
